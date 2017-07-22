dd1b49c5cb19acf93c6d648c577f7f1f6f70fc5d
Godfrey Best <gcb246@stern.nyu.edu>
2017-07-22 14:00:45 -0400

    next redirect added

--- redditclone/accounts/views.py
+++ redditclone/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
@@ -24,6 +24,9 @@ def loginView(request):
 		user = authenticate(username=request.POST['username'], password=request.POST['password'])
 		if user is not None:
 			login(request, user)
+			# Check if there is a next parameter
+			if 'next' in request.POST:
+				return redirect(request.POST['next'])
         	# Redirect to a success page.
 			return render(request, 'accounts/login.html', {'error': "Login Successful!"})
 		else:
