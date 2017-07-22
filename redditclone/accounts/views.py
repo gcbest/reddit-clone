b63315529f4c9686cd41438ca6381893769de4c1
Godfrey Best <gcb246@stern.nyu.edu>
2017-07-22 12:24:48 -0400

    include accounts url

--- redditclone/accounts/views.py
+++ redditclone/accounts/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def signup(request):
 	if request.method == 'POST':
 		if request.POST['password1'] == request.POST['password2']:
-			User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-			return render(request, 'accounts/signup.html')
+			# check to see if the username already exists
+			try:
+				User.objects.get(username=request.POST['username'])
+				return render(request, 'accounts/signup.html', {'error': "Username has already been taken"})
+			except User.DoesNotExist:
+				user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+				login(request, user)
+				return render(request, 'accounts/signup.html')
 		else:
 			return render(request, 'accounts/signup.html', {'error': "Passwords didn't match"})
 	else:
 		return render(request, 'accounts/signup.html')
+
+def loginView(request):
+	if request.method == 'POST':
+		user = authenticate(username=request.POST['username'], password=request.POST['password'])
+		if user is not None:
+			login(request, user)
+        	# Redirect to a success page.
+			return render(request, 'accounts/login.html', {'error': "Login Successful!"})
+		else:
+			return render(request, 'accounts/login.html', {'error': "Username and Password didn't match"})
+	else:
+		return render(request, 'accounts/login.html')
