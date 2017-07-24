335d218af71ff5fe15c89af07f13cbbd1b3970eb
Godfrey Best <gcb246@stern.nyu.edu>
2017-07-24 16:20:04 -0400

    saving post

--- redditclone/posts/models.py
+++ redditclone/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	url = models.TextField()
+	pub_date = models.DateTimeField()
+	# to connect the posts to a specific user
+	author = models.ForeignKey(User)
+	votes_totale = models.IntegerField(default=1)
--- redditclone/posts/views.py
+++ redditclone/posts/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from . import models
 
 @login_required
 # Create your views here.
 def create(request):
-	return render(request, 'posts/create.html')
+	if request.method == 'POST':
+		if request.POST['title'] and request.POST['url']:
+			post = models.Post()
+			post.title = request.POST['title']
+			post.url = request.POST['url']
+			post.pub_date = timezone.datetime.now()
+			post.author = request.user
+			post.save()
+		return render(request, 'posts/create.html')
+	else:
+		return render(request, 'posts/create.html')
